1c7ad0b73c955df9d89281bddad417db61248da7
Jugnu Agrawal <jugnu@appveen.com>
2020-12-07 14:43:24 +0530

    #34

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const log4js = require("log4js");
 global.version = require("./package.json").version
 let d = new Date();
@@ -62,55 +64,68 @@ let dataFileName = `odp_backup_restore_${d}_data.log`
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-const odp_fetch = require("./odp/fetch")
-const odp_customise = require("./odp/customise")
-const odp_upsert = require("./odp/upsert")
-const odp_delete = require("./odp/delete")
-const backup = require("./utils/backupHandler")
 const misc = require("./utils/misc")
 const cli = require("./cli/cli")
 
-misc.header(`ODP Config. Import/Export Utility ${global.version}`);
-
-cli.pickMode()
-    .then(_mode => {
-        if (_mode == "Backup") _backup()
-        if (_mode == "Restore") _restore()
-        if (_mode == "Delete All") _delete()
+if (!fs.existsSync(path.join(process.cwd(), 'config.json'))) {
+    misc.print('config.json', 'No Found, Creating One.');
+    return cli.init().then(data => {
+        fs.writeFileSync(path.join(process.cwd(), 'config.json'), JSON.stringify(data, null, 2), 'utf-8');
+        return start();
     })
-
-function _backup() {
-    backup.init();
-    odp_fetch.login()
-    .then(() => odp_fetch.startMapping())
-    .then(() => clearInterval(global.refreshIntervalID))
-    .then(() => odp_customise.customBackup())
+} else {
+    return start();
 }
 
-function _restore() {
-    backup.restoreInit();
-    odp_upsert.login()
-        .then(() => odp_upsert.upsertLibrary())
-        .then(() => odp_upsert.upsertDataFormats())
-        .then(() => odp_upsert.upsertDataServices())
-        .then(() => odp_upsert.upsertRoles())
-        .then(() => odp_upsert.upsertNanoServices())
-        .then(() => odp_upsert.upsertAgents())
-        .then(() => odp_upsert.upsertPartnerSecrets())
-        .then(() => odp_upsert.upsertPartners())
-        .then(() => odp_upsert.upsertFlows())
-        .then(() => odp_upsert.upsertBookmarks())
-        .then(() => odp_upsert.upsertGroups())
-        .then(() => clearInterval(global.refreshIntervalID))
-        .then(() => misc.header('Restore complete!'))
-}
+function start() {
+    const odp_fetch = require("./odp/fetch")
+    const odp_customise = require("./odp/customise")
+    const odp_upsert = require("./odp/upsert")
+    const odp_delete = require("./odp/delete")
+    const backup = require("./utils/backupHandler")
 
-function _delete() {
-    odp_delete.login()
-        .then(_ => odp_delete.deleteGroups())
-        .then(_ => odp_delete.deleteBookmarks())
-        .then(_ => odp_delete.deleteFlows())
-        .then(_ => odp_delete.deleteDataServices())
-        .then(_ => odp_delete.deleteLibrary())
-        .then(() => clearInterval(global.refreshIntervalID));
+    misc.header(`ODP Config. Import/Export Utility ${global.version}`);
+
+    cli.pickMode()
+        .then(_mode => {
+            if (_mode == "Backup") _backup()
+            if (_mode == "Restore") _restore()
+            if (_mode == "Delete All") _delete()
+        })
+
+    function _backup() {
+        backup.init();
+        odp_fetch.login()
+            .then(() => odp_fetch.startMapping())
+            .then(() => clearInterval(global.refreshIntervalID))
+            .then(() => odp_customise.customBackup())
+    }
+
+    function _restore() {
+        backup.restoreInit();
+        odp_upsert.login()
+            .then(() => odp_upsert.upsertLibrary())
+            .then(() => odp_upsert.upsertDataFormats())
+            .then(() => odp_upsert.upsertDataServices())
+            .then(() => odp_upsert.upsertRoles())
+            .then(() => odp_upsert.upsertNanoServices())
+            .then(() => odp_upsert.upsertAgents())
+            .then(() => odp_upsert.upsertPartnerSecrets())
+            .then(() => odp_upsert.upsertPartners())
+            .then(() => odp_upsert.upsertFlows())
+            .then(() => odp_upsert.upsertBookmarks())
+            .then(() => odp_upsert.upsertGroups())
+            .then(() => clearInterval(global.refreshIntervalID))
+            .then(() => misc.header('Restore complete!'))
+    }
+
+    function _delete() {
+        odp_delete.login()
+            .then(_ => odp_delete.deleteGroups())
+            .then(_ => odp_delete.deleteBookmarks())
+            .then(_ => odp_delete.deleteFlows())
+            .then(_ => odp_delete.deleteDataServices())
+            .then(_ => odp_delete.deleteLibrary())
+            .then(() => clearInterval(global.refreshIntervalID));
+    }
 }
